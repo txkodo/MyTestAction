82f79abbd2c4d20b0580912d753930b6c22f7430
BuildTools <unconfigured@null.spigotmc.org>
2024-01-07 23:01:54 +0900

    log dirpaths

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const core = require('@actions/core');
 const fs = require('fs');
 
+function recursivePath(path){
+    if (fs.statSync(path).isDirectory()){
+        return Object.fromEntries(fs.readdirSync(path).map(x => [x,recursivePath(x)]))
+    }
+    return path
+}
+
 try {
     const path = core.getInput('json-path');
     const content = fs.readFileSync(path,{encoding:"utf8"})
     const jcontent = JSON.parse(content)
     console.log(content)
+
+    console.log(recursivePath("dist"))
+
     if (jcontent.version === undefined){
         core.setFailed("version number not found");
     }
