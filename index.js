7c08a602deef3732441b8f50ef17d88f9ce2d47d
BuildTools <unconfigured@null.spigotmc.org>
2024-01-07 21:44:44 +0900

    set json encodeing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 try {
     const path = core.getInput('json-path');
-    const content = fs.readFileSync(path)
+    const content = fs.readFileSync(path,{encoding:"utf8"})
     const jcontent = JSON.parse(content)
     console.log(content)
     core.setOutput("version", jcontent.version);
