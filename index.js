2bdfb40c388a9e64f8c1e7d40d3909e77f88ef50
BuildTools <unconfigured@null.spigotmc.org>
2024-01-07 21:46:22 +0900

    fixed

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ try {
     const content = fs.readFileSync(path,{encoding:"utf8"})
     const jcontent = JSON.parse(content)
     console.log(content)
-    core.setOutput("version", jcontent.version);
-    if (version === undefined){
+    if (jcontent.version === undefined){
         core.setFailed("version number not found");
     }
+    core.setOutput("version", jcontent.version);
 } catch (error) {
   core.setFailed(error.message);
 }
