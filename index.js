afd91e6e6c834ae8e93933a85034b7a428660186
BuildTools <unconfigured@null.spigotmc.org>
2024-01-15 01:36:55 +0900

    ✨

--- index.js
+++ index.js
@@ -2,25 +2,43 @@ const core = require('@actions/core');
 const fs = require('fs');
 const path = require('path');
 
-function recursivePath(p){
-    if (fs.statSync(p).isDirectory()){
-        return Object.fromEntries(fs.readdirSync(p).map(x => [x,recursivePath(path.join(p,x))]))
-    }
-    return null
-}
-
 try {
-    const path = core.getInput('json-path');
-    const content = fs.readFileSync(path,{encoding:"utf8"})
-    const jcontent = JSON.parse(content)
-    console.log(content)
+    const path = core.getInput('json-path',{trimWhitespace:true}) ?? "package.json";
+    const content = JSON.parse(fs.readFileSync(path,{encoding:"utf8"}))
+    
+    let [major, minor, patch] = content.version.split('.').map(parseInt);
 
-    console.log(recursivePath("dist"))
+    let release_type = core.getInput('release-type',{trimWhitespace:true})
 
-    if (jcontent.version === undefined){
-        core.setFailed("version number not found");
+    core.setOutput("current-version",`${major},${minor},${patch}`)
+
+    switch (release_type){
+        case "major":
+            major += 1
+            minor = 0
+            patch = 0
+            break;
+        case "minor":
+            minor += 1
+            patch = 0
+            break;
+        case "patch":
+        default:
+            patch += 1
+            break;
     }
-    core.setOutput("version", jcontent.version);
+
+    const next_version = `${major},${minor},${patch}`
+    core.setOutput("next-version",next_version)
+
+    const updateJsonFile = core.getInput('update-json',{trimWhitespace:true}).toLowerCase() === "true";
+
+    if (updateJsonFile){
+        content.version = next_version
+        await fs.writeFile('package.json', JSON.stringify(content, undefined, 2), {
+            encoding: 'utf8',
+            });
+    }
 } catch (error) {
   core.setFailed(error.message);
 }
